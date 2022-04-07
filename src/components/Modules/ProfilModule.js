a857205aafa9b455d782c4738c731209fcbf6c13
gillancodes <gillancodes@laposte.net>
2022-04-07 15:08:16 +0200

    image path fixed

--- src/components/Modules/ProfilModule.js
+++ src/components/Modules/ProfilModule.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProfilModule({user}) {
   return (
     <div className='ProfilModule'>
-        <img src={user.userPic} alt="" />
+        <img src={user.userPic.imageUrl} alt="" />
         <a href={`/${user.username}`}>{user.displayName ? user.displayName : user.username}</a>
 
     </div>
