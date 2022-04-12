4d78525f1ce2c77a40855c3efaecc0cac11149a3
gillancodes <gillancodes@laposte.net>
2022-04-12 13:54:21 +0200

    Added: Contact Page

--- src/components/static/Contact.js
+++ src/components/static/Contact.js
@@ -2,6 +2,38 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-container'>
+      
+      <h1>Nous Contacter</h1>
+
+      <div className="fields">
+        <div className="field">
+          <span>Email*</span>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="field">
+          <span>Pseudo</span>
+          <input type="text" name="pseudo" id="pseudo" />
+        </div>
+        <div className="field">
+          <span>Contenu de l'e-mail*</span>
+          <textarea name="content" id="content" required></textarea>
+        </div>
+        <div className="field">
+          <button className='send'>Envoyer</button>
+        </div>
+      </div>
+
+     
+      <div className="social">
+        <h2>Nos réseaux sociaux</h2>
+        <div className="links">
+          <a href="https://twitter.com/opencodess">Twitter</a>
+          <a href="mailto:opencodess@gmail.com">Mail</a>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
