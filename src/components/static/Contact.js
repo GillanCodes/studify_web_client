263322b6d14b54dca8de6589abe7af34cad5e34c
gillancodes <gillancodes@laposte.net>
2022-04-12 14:20:07 +0200

    Updated : Api handle

--- src/components/static/Contact.js
+++ src/components/static/Contact.js
@@ -1,26 +1,52 @@
-import React from 'react'
+import { SelectUnstyledContext } from '@mui/base';
+import axios from 'axios';
+import React, { useState } from 'react'
 
 export default function Contact() {
+
+  const [email, setEmail] = useState("");
+  const [pseudo, setPseudo] = useState("");
+  const [content, setContent] = useState("");
+  const [send, setSend] = useState(false);
+  
+  const sendHandle = () => {
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}/api/contact`,
+      data: {
+        email, pseudo, content
+      }
+    }).then(() => {
+      setSend(true);
+      setContent('');
+      setEmail('');
+      setPseudo('');
+    }) 
+
+  }
+
   return (
     <div className='contact-container'>
       
       <h1>Nous Contacter</h1>
 
       <div className="fields">
+      {send && (<p>Message Envoyé !</p>)}
         <div className="field">
           <span>Email*</span>
-          <input type="email" name="email" id="email" required />
+          <input type="email" name="email" id="email" value={email} required  onChange={(e) => setEmail(e.target.value)}/>
         </div>
         <div className="field">
           <span>Pseudo</span>
-          <input type="text" name="pseudo" id="pseudo" />
+          <input type="text" name="pseudo" id="pseudo" value={pseudo}  onChange={(e) => setPseudo(e.target.value)}/>
         </div>
         <div className="field">
           <span>Contenu de l'e-mail*</span>
-          <textarea name="content" id="content" required></textarea>
+          <textarea name="content" id="content" value={content} required onChange={(e) => setContent(e.target.value)}></textarea>
         </div>
         <div className="field">
-          <button className='send'>Envoyer</button>
+          <button className='send' onClick={sendHandle}>Envoyer</button>
         </div>
       </div>
 
