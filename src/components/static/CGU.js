da6cee3b309b1b35a8cb76b97617cb7388186fad
gillancodes <gillancodes@laposte.net>
2022-04-20 15:07:30 +0200

    Added & Ended CGU Pages + Style

--- src/components/static/CGU.js
+++ src/components/static/CGU.js
@@ -2,6 +2,85 @@ import React from 'react'
 
 export default function CGU() {
   return (
-    <div>CGU</div>
+    <div className='cgu-container container'>
+      <div className="content">
+        <h1>Conditions Générales d'Utilisation</h1>
+        <p>
+          <span>Article 1 : Objet</span> <br />
+          Les présentes CGU ou Conditions Générales d’Utilisation encadrent juridiquement l’utilisation des services du site Studify (ci-après dénommé « le site »). <br />
+          Constituant le contrat entre la société Codeed, l’Utilisateur, l’accès du site doit être précédé de l’acceptation de ces CGU. L’accès à cette plateforme signifie l’acceptation des présentes CGU. <br />
+        </p>
+        <p>
+          <span>Article 2 : Mentions Légales</span> <br />
+          L’édition du site Studify est assuré par la société Codeed inscrite sous le numéro, dont le siège social est localisé au 6 Rue du Général Leclerc, 78600, le Mesnil le Roi, France Métropolitaine. <br />
+          L’hébergeur du site Studify.codeed.fr est la société OVH, sise au 2 rue Kellermann, 59100 Roubaix, France. <br />
+        </p>
+        <p>
+        <span>Article 3 : Accès au site</span> <br />
+          Le site Studify permet d’accéder gratuitement aux services suivants : <br />
+          <ul>
+            <li>Création d’un compte</li>
+            <li>Personnalisation du compte</li>
+            <li>Création et partage de documents</li>
+            <li>Création et partage de Quizz</li>
+          </ul> <br />
+          Le site est accessible gratuitement n’importe où par tout utilisateur disposant d’un accès à internet. Tous les frais nécessaires pour l’accès aux services (matériel informatique, connexion Internet …) sont à charge de l’utilisateur.
+          L’accès au site Studify peut être interdit à un compte dans le cas où celui-ci ne respect pas les CGU ou Condition Générales d’Utilisation
+          L’accès aux sévices dédiés aux membres s’effectue à l’aide d’un identifiant (pseudo), d’une adresse mail valide et d’un mot de passe.
+          Pour des raisons de maintenances ou autres, l’accès au site peut être interrompu ou suspendu par l’éditeur sans préavis ni justification.
+        </p>
+        <p>
+        <span>Article 4 : Collecte des données</span> <br />
+        Pour la création du compte de l’Utilisateur, la collecte des informations au moment de l’inscription sur le site est nécessaire et obligatoire. Conformément à la loi n°78-17 du 6 janvier relative à l’informatique, aux fichiers et aux libertés, la collecte et le traitement des données personnelles s’effectuent dans le respect de la vie privée.
+        Suivant la loi Informatique et Libertés en date du 6 janvier 1978, articles 39 et 40, l’Utilisateur dispose du droit d’accéder, de rectifier, de supprimer et d’opposer ses données personnelles. L’exercice de ce droit s’effectue par :
+        <ul>
+          <li>Le formulaire de contact</li>
+          <li>Son espace client</li>
+        </ul> <br />
+        </p>
+        <p>
+        <span>Article 5 : Propriété intellectuelle</span> <br />
+        Les marques, logos ainsi que les contenus du site Studify (Illustrations, textes, …) sont protégés par le Code de la propriété intellectuelle et par le droit d’auteur. <br />
+        La reproduction et la copie des contenus par l’Utilisateur requièrent une autorisation préalable du site. Dans ce cas, toute utilisation à des usages commerciaux ou à des fins publicitaires est proscrite. <br />
+        </p>
+        <p>
+        <span>Article 6 : Responsabilité</span>  <br />
+        Bien que les informations publiées sur le site soient réputées fiables, le site se réserve la faculté d’une non-garantie de la fiabilité des sources.  <br />
+        Le site Studify est communautaire. La diffusion de fausses informations provenant des Utilisateurs ne peut pas tenir pour responsable Codeed. Le site Studify a mis en place un système de modération humain qui permet aux Utilisateurs une navigation plutôt saine. <br />
+        Le site décline toute responsabilité concernant les éventuels virus pouvant infecter le matériel informatique de l’Utilisateur après l’utilisation ou l’accès à ce site. <br />
+        Le site ne peut être tenu pour responsable en cas de force majeure ou du fait imprévisible et insurmontable d’un tiers. <br />
+        La garantie totale de la sécurité et la confidentialité des données n’est pas assurée par le site. Cependant, le site s’engage à mettre en œuvre toutes les méthodes requises pour le faire au mieux. <br />
+        </p>
+        <p>
+        <span>Article 7 : Liens hypertextes</span> <br />
+        Le site peut être constitué de liens hypertextes. En cliquant sur ces derniers, l’Utilisateur sortira de la plateforme. Cette dernière n’a pas de contrôle et ne peut pas être tenue responsable du contenu des pages web relatives à ces liens. <br />
+        </p>
+        <p>
+        <span>Article 8 : Cookies</span> <br />
+        Lors des visites sur le site, l’installation automatique d’un cookie sur le logiciel de navigation de l’Utilisateur peut survenir. <br />
+        Les cookies correspondent à de petits fichiers déposés temporairement sur le disque dur de l’ordinateur de l’Utilisateur. Ces cookies sont nécessaires pour assurer l’accessibilité et la navigation sur le site. Ces fichiers ne comportent pas d’informations personnelles et ne peuvent pas être utilisés pour l’identification d’une personne. <br />
+        L’information présente dans les cookies est utilisée pour améliorer les performances de navigation sur le site studify.codeed.fr. <br />
+        En naviguant sur le site, l’Utilisateur accepte les cookies. Leur désactivation peut s’effectuer via les paramètres du logiciel de navigation. <br />
+        </p>
+        <p>
+        <span>Article 9 : Publication par l’Utilisateur</span> <br />
+        Le site Studify permet aux membres de publier des documents, images, liens et textes. <br />
+        Dans ses publications, le membre est tenu de respecter les règles de la Netiquette ainsi que les règles de droit en vigueur. <br />
+        Le site dispose du droit d’exercer une modération à priori sur les publications et peut refuser leur mise en ligne sans avoir à fournir de justification. <br />
+        Le membre garde l’intégralité de ses droits de propriété intellectuelle. Toutefois, toute publication sur le site implique la délégation du droit non exclusif et gratuit à la société éditrice de représenter, reproduire, modifier, adapter, distribuer et diffuser la publication n’importe où et sur n’importe quel support pour la durée de la propriété intellectuelle. Cela peut se faire directement ou par l’intermédiaire d’un tiers autorisé. Cela concerne notamment le droit d’utilisation de la publication sur le web et sur les réseaux de téléphonie mobile. <br />
+        À chaque utilisation, l’éditeur s’engage à mentionner le nom du membre à proximité de la publication. <br />
+        L’Utilisateur est tenu responsable de tout contenu qu’il met en ligne. L’Utilisateur s’engage à ne pas publier de contenus susceptibles de porter atteinte aux intérêts de tierces personnes. Toutes procédures engagées en justice par un tiers lésé à l’encontre du site devront être prises en charge par l’Utilisateur. <br />
+        La suppression ou la modification par le site du contenu de l’Utilisateur peut s’effectuer à tout moment, pour n’importe quelle raison et sans préavis <br />
+        </p>
+        <p>
+        <span>Article 10 : Durée du contrat</span> <br />
+        Le présent contrat est valable pour une durée indéterminée. Le début de l’utilisation des services du site marque l’application du contrat à l’égard de l’Utilisateur. <br />
+        </p>
+        <p>
+        <span>Article 11 : Droit applicable et juridiction compétente</span> <br />
+        Le présent contrat est soumis à la législation française. L’absence de résolution à l’amiable des cas de litige entre les parties implique le recours aux tribunaux français compétents pour régler le contentieux. <br />
+        </p>
+      </div>
+    </div>
   )
 }
